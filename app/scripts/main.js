20be0062093f2811ed13a4c5a605f0f86ca77c05
雅 任 <yaren@RENYAdeMacBook-Air.local>
2018-05-01 18:28:47 +0800

    宁皓转移

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+  // Your custom JavaScript goes here
+/**
+ * narbar
+
+$('ui.navbar .search-icon > i')
+  .click(function () {
+    let icon = $(this)
+    if (icon.text() === 'search'){
+      icon.text('close')
+    }else {
+      icon.text('search')
+    }
+
+    })
+ */
+ $('.ui.navbar .search-icon >i')
+  .click(function(){
+    let icon = $(this)
+    if(icon.text() === 'search'){
+      icon.text('close').parents('.content').addClass('searching')
+    }else {
+      icon.text('search').parents('.content').removeClass('searching')
+    }
+  })
+
+
+
+})();
