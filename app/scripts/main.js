92954f769f765ebb22d0697f71bd9ec179c1de92
雅 任 <yaren@RENYAdeMacBook-Air.local>
2018-05-03 11:28:56 +0800

    完成英雄区

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,8 +38,23 @@ $('ui.navbar .search-icon > i')
   .clone()
   .appendTo('.ui.sidebar')
 
+/**
+ * carousel
+ */
   $('.ui.hero.carousel')
-  .slick()
+  .slick({
+    // 'arrows':false,
+    nextArrow : '<button class="ui next button"><i class="material-icons">chevron_right</i></button>',
+    prevArrow : '<button class="ui prev button"><i class="material-icons">chevron_left</i></button>',
+    responsive:[
+      {
+        breakpoint:767,
+        settings:{
+          dots:true
+        }
+      }
+    ]
+  })
 
 
 })();
